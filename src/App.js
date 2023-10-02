cfd8f5ccf362075b18efca9824fc79d306689127
Shivam Rajput <rshivam973@gmail.com>
2023-10-03 01:40:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [note,netNote] = useState([]);
+
   return (
     <div className="App">
         <p className='main-heading'>Notes App</p>
